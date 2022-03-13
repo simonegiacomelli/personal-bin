ccd35bf9d11942722b62a759d66b81f719fdf420
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-03-13 07:55:55 +0100

    quick display change resolution

--- disp.kts
+++ disp.kts
@@ -1,9 +1,18 @@
 #!/usr/bin/env kscript
 import java.io.File
 import java.util.concurrent.TimeUnit
-import java.util.Properties
 
-private val resolutions_str = """
+private class Display {
+
+    private class Resolution(properties: Map<String, String>) {
+        val resolution: String by properties
+        private val parts = resolution.split("x")
+        val x = parts[0]
+        val y = parts[1]
+        override fun toString(): String = "$resolution ($x $y)"
+    }
+
+    private val resolutions_str = """
         resolution: 1920x1200, refresh rate: 0, HiDPI: True
         resolution: 1680x1050, refresh rate: 0, HiDPI: True
         resolution: 1440x900, refresh rate: 0, HiDPI: True
@@ -13,15 +22,15 @@ private val resolutions_str = """
         resolution: 720x450, refresh rate: 0, HiDPI: True
 """.trimIndent()
 
-private class Display {
-    private class Resolution(properties: Map<String, String>) {
-        val resolution: String by properties
-        override fun toString(): String = "$resolution"
+    private val resolutions = decodeResolutions(resolutions_str)
+    private fun help() {
+        println("Please, specify a valid index for a resolution:")
+        resolutions.forEachIndexed { index, res ->
+            println(" $index: $res")
+        }
     }
 
-    private val resolutions = decodeResolutions(resolutions_str)
-
-    private fun decodeResolutions(content: String): List<Resolution> =
+    private fun decodeResolutions(content: String) =
         content.split("\n")
             .map { it.trim() }
             .map { line ->
@@ -32,72 +41,58 @@ private class Display {
                 Resolution(properties)
             }
 
-
     fun main(args: Array<String>) {
         val arg0 = args.firstOrNull()
         if (arg0 == null) {
             help()
             return
         }
-        val res = resolutions.getOrNull(arg0.toInt()) ?: kotlin.run {
+        val res: Resolution = resolutions.getOrNull(arg0.toInt()) ?: kotlin.run {
             println("Index not found")
+            return
         }
-        "display_manager.py res "
-        val pids = getPidsForTcpPort(args.first().toInt())
-        pids.forEach { pid ->
-            println("Killing $pid")
-            "kill $pid".runCommand().also { }
-        }
+        val cmd = "display_manager.py res ${res.x} ${res.y}"
+        println(cmd)
+        cmd.runCommand()
+
     }
 
+    fun String.runCommand(): String {
 
-    private fun help() {
-        println("Please, specify a valid index for a resolution:")
-        resolutions.forEachIndexed { index, res ->
-            println(" $index: $res")
+        val workingDir = File(".")
+        val proc = ProcessBuilder(*split(" ").toTypedArray())
+            .directory(workingDir)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+
+        proc.waitFor(1, TimeUnit.MINUTES)
+        return proc.inputStream.bufferedReader().readText()
+    }
+
+    fun getPidsForTcpPort(port: Int): List<String> {
+        val lsofOutput = "lsof -i tcp:$port".runCommand()
+
+        if (lsofOutput.isBlank()) {
+            println("No process is using $port")
+            return emptyList()
         }
+
+        println(lsofOutput)
+        val lines = lsofOutput.split("\n")
+
+        val header = lines.first()
+        val headerValues = header.splitLines()
+        check(headerValues[1] == "PID") { "expected header PID in `$header`" }
+        val procs = lines.drop(1).filter { it.isNotBlank() }
+        val pids = procs.map { it.splitLines()[1] }
+        return pids
     }
 
+    fun String.splitLines() = this.split(" ").filter { it.isNotBlank() }
+
 }
-
-
-
-
-
-fun String.runCommand(): String {
-
-    val workingDir = File(".")
-    val proc = ProcessBuilder(*split(" ").toTypedArray())
-        .directory(workingDir)
-        .redirectOutput(ProcessBuilder.Redirect.PIPE)
-        .redirectError(ProcessBuilder.Redirect.PIPE)
-        .start()
-
-    proc.waitFor(1, TimeUnit.MINUTES)
-    return proc.inputStream.bufferedReader().readText()
-}
-
-fun getPidsForTcpPort(port: Int): List<String> {
-    val lsofOutput = "lsof -i tcp:$port".runCommand()
-
-    if (lsofOutput.isBlank()) {
-        println("No process is using $port")
-        return emptyList()
-    }
-
-    println(lsofOutput)
-    val lines = lsofOutput.split("\n")
-
-    val header = lines.first()
-    val headerValues = header.splitLines()
-    check(headerValues[1] == "PID") { "expected header PID in `$header`" }
-    val procs = lines.drop(1).filter { it.isNotBlank() }
-    val pids = procs.map { it.splitLines()[1] }
-    return pids
-}
-
-fun String.splitLines() = this.split(" ").filter { it.isNotBlank() }
-
 
 
 Display().main(args)
+
