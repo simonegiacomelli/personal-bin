a5ad78aa971ba52e1df7be3e3949f8aabfe907f2
simone <simone.giacomelli@gmail.com>
2024-11-20 14:24:32 +0000

    brightness_control_intel.py

--- brightness_control_intel.py
+++ brightness_control_intel.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python3
-
-import os
+import math
 import sys
 from datetime import datetime
+from pathlib import Path
 
-# Constants
-BRIGHTNESS_PATH = "/sys/class/backlight/intel_backlight/brightness"
-MAX_BRIGHTNESS_PATH = "/sys/class/backlight/intel_backlight/max_brightness"
-LOG_FILE = "/tmp/brightness.log"
-STEP = 52
+BACKLIGHT_PATH = Path("/sys/class/backlight/intel_backlight")
+
 
 def get_variable(filename):
-    """Read and return the content of a file as an integer."""
-    with open(filename, "r") as f:
-        return int(f.read().strip())
+    return int(Path(BACKLIGHT_PATH / filename).read_text().strip())
 
-def log_message(message):
+
+def set_variable(filename, value):
+    Path(BACKLIGHT_PATH / filename).write_text(str(value))
+
+
+def log(message):
     """Write a message to the log file."""
-    with open(LOG_FILE, "a") as log:
+    with open('/tmp/brightness.log', "a") as log:
         log.write(f"{datetime.now()} - {message}\n")
 
+
 def main():
-    """Main function to handle brightness adjustment."""
     if len(sys.argv) < 2:
-        log_message(f"Error: No arguments provided. Args: {sys.argv}")
+        log(f"Error: No arguments provided. Args: {sys.argv}")
         return
 
     event = sys.argv[1].lower()  # ACPI event passed as argument
-    current_brightness = get_variable(BRIGHTNESS_PATH)
+    current_brightness = get_variable('brightness')
+    max_brightness = get_variable('max_brightness')
+    step = math.ceil(max_brightness / 10)
 
     if "brightnessup" in event:
-        new_brightness = min(current_brightness + STEP, get_variable(MAX_BRIGHTNESS_PATH))
+        new_brightness = min(current_brightness + step, max_brightness)
     elif "brightnessdown" in event:
-        new_brightness = max(current_brightness - STEP, 0)
+        new_brightness = max(current_brightness - step, 0)
     else:
-        log_message(f"Error: Unknown event '{event}'. Args: {sys.argv}")
+        log(f"Error: Unknown event '{event}'. Args: {sys.argv}")
         return
 
-    with open(BRIGHTNESS_PATH, "w") as f:
-        f.write(str(new_brightness))
+    set_variable('brightness', new_brightness)
 
-    log_message(
-        f"Event-x: {event}, Current: {current_brightness}, New: {new_brightness}, Args: {sys.argv}"
-    )
+    log(f"Event: {event}, Current: {current_brightness}, New: {new_brightness}, Args: {sys.argv}")
+
 
 if __name__ == "__main__":
     main()
