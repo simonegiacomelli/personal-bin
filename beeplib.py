f4aa22cdea954becdcdaecd1efca95e126cbf90b
simone <simone.giacomelli@gmail.com>
2024-12-08 16:18:30 +0000

    record/replay with pynput/dotool

---
+++ beeplib.py
@@ -0,0 +1,11 @@
+import os
+
+
+def beep():
+    try:
+        os.system('play -n synth 0.5 sine 1000 gain -10')
+    except:
+        print('Beep failed, please install sox with')
+        print('sudo apt-get install sox')
+
+
