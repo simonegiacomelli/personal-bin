824165e0db0d7f204c08c14d1d25d0fec4260738
simone <simone.giacomelli@gmail.com>
2024-12-08 17:24:49 +0000

    irp_record / irp_replay

--- beeplib.py
+++ beeplib.py
@@ -3,7 +3,7 @@ import os
 
 def beep():
     try:
-        os.system('play -n synth 0.5 sine 1000 gain -10')
+        os.system('play -n synth 0.5 sine 1000 gain -10 > /dev/null 2>&1')
     except:
         print('Beep failed, please install sox with')
         print('sudo apt-get install sox')
